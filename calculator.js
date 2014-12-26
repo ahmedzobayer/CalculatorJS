4ed8f02bd7075d58b354d814cbeaa7c35616c499
ahmedzobayer <ahmedzobayer@gmail.com>
2014-12-26 18:12:46 +0600

    calculator

--- calculator.js
+++ calculator.js
@@ -4,6 +4,11 @@ function disp(val)
 	document.frm.display.value+= val;
 }
 
+function clr()
+{
+	document.frm.display.value="";
+}
+
 function calc()
 {
 	document.frm.display.value=eval(document.frm.display.value);
